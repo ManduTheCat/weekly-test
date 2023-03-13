0f3f6ea6d33aa0171bff4f93283ae9f6d2f8cdf1
subinan <53176366+subinan@users.noreply.github.com>
2023-03-13 13:34:41 +0900

    Update BJ_20125_쿠키의신체측정.java

--- week17/Subin/BJ_20125_쿠키의신체측정.java
+++ week17/Subin/BJ_20125_쿠키의신체측정.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class Main {
+public class BJ_20125_쿠키의신체측정 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
