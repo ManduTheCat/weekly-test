f2091f6728ec70fdcffdda8d4b6a1c805118222f
subinan <53176366+subinan@users.noreply.github.com>
2023-07-01 21:17:24 +0900

    Create BJ_20499_Darius님한타안함?.java

---
+++ week24/Subin/BJ_20499_Darius님한타안함?.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), "/");
+		
+		int k = Integer.parseInt(st.nextToken());
+		int d = Integer.parseInt(st.nextToken());
+		int a = Integer.parseInt(st.nextToken());
+		
+		if (d == 0 || k + a < d) {
+			System.out.println("hasu");
+		} else {
+			System.out.println("gosu");
+		}
+	}
+	
+}
